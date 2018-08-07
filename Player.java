11d03a68ee1f4d5b29c5886d47cde826d2a910eb
narmathaa18 <34527401+narmathaa18@users.noreply.github.com>
2018-08-07 15:18:51 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,18 @@
+public class Player {
+
+    int sets;
+    int games;
+    int score;
+    int points;
+    Player() {
+        sets = 0;
+        games = 0;
+        score = 0;
+        points = 0;
+    }
+    Player(int sets,int games, int score){
+        this.sets = sets;
+        this.games = games;
+        this.score = score;
+    }
+}
